2142a8aaa574a8c66aebff742446cbbd15b929a4
Bryson Goins <brysonmg@gmail.com>
2021-08-02 16:24:58 -0500

    working on summary page

--- StoreWorks/client/src/components/Summary.js
+++ StoreWorks/client/src/components/Summary.js
@@ -1,5 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getAllSales } from '../modules/saleManager';
+import { getAllReceived } from '../modules/receivedManager';
+import { getAllShrink } from '../modules/shrinkManager';
+
 
 export const Summary = () => {
+    const [sales, setSales] = useState([]);
+    const [received, setReceived] = useState([]);
+    const [shrink, setShrink] = useState([]);
 
+    const getSales = () => {
+        getAllSales().then(allSales => {
+            setSales(allSales)
+        })
+    }
+
+    const getReceived = () => {
+        getAllReceived().then(allReceived => {
+            setReceived(allReceived)
+        })
+    }
+
+    const getShrink = () => {
+        getAllShrink().then(allShrink => {
+            setShrink(allShrink)
+        })
+    }
+
+    useEffect(() => {
+        getSales();
+        getReceived();
+        getShrink();
+    }, [])
 }
