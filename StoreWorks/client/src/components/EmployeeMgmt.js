86d6a08983b19a661f4b60c9dd77cc5c317dfc9e
Bryson Goins <brysonmg@gmail.com>
2021-08-05 12:05:33 -0500

    all tables draggable + styling changes

--- StoreWorks/client/src/components/EmployeeMgmt.js
+++ StoreWorks/client/src/components/EmployeeMgmt.js
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import { getAllEmployees } from '../modules/employeeManager';
 import { EmployeeCard } from './EmployeeCard';
+import Draggable from 'react-draggable';
 import '../styles/tables.css';
 import '../styles/employeeManage.css';
 
 export const EmployeeMgmt = () => {
     const [employees, setEmployees] = useState([]);
+
+    const checkIfMobileUser = agent => {
+        if (agent.includes('mobi')) {
+            return true
+        }
+        return false
+    }
+
+    let userAgent = navigator.userAgent.toLowerCase(),
+        userIsOnMobile = checkIfMobileUser(userAgent);
 
     const getEmployees = () => {
         getAllEmployees().then(allEmployees => {
@@ -18,22 +29,24 @@ export const EmployeeMgmt = () => {
     }, [])
 
     return (
-        <div className="employeeMgmt">
-            <table className="employeeList">
-                <thead>
-                    <tr className="tableSubLabel">
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Manager Access</th>
-                        <th>Controls</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {employees.map(employee => {
-                        return <EmployeeCard key={employee.id} employee={employee} getEmployees={getEmployees} />
-                    })}
-                </tbody>
-            </table>
-        </div>
+        <Draggable disabled={userIsOnMobile}>
+            <div className="employeeMgmt">
+                <table className="employeeList">
+                    <thead>
+                        <tr className="tableSubLabel">
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Manager Access</th>
+                            <th>Controls</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {employees.map(employee => {
+                            return <EmployeeCard key={employee.id} employee={employee} getEmployees={getEmployees} />
+                        })}
+                    </tbody>
+                </table>
+            </div>
+        </Draggable>
     )
 }
--- StoreWorks/client/src/components/ProductMgmt.js
+++ StoreWorks/client/src/components/ProductMgmt.js
@@ -2,12 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 import { getAllProducts } from '../modules/productManager';
 import { ProductCard } from './ProductCard';
+import Draggable from 'react-draggable';
 import '../styles/tables.css';
 import '../styles/productManage.css';
 
 export const ProductMgmt = () => {
     const [products, setProducts] = useState([])
     const history = useHistory();
+
+    const checkIfMobileUser = agent => {
+        if (agent.includes('mobi')) {
+            return true
+        }
+        return false
+    }
+
+    let userAgent = navigator.userAgent.toLowerCase(),
+        userIsOnMobile = checkIfMobileUser(userAgent);
 
     const getProducts = () => {
         getAllProducts().then(allProducts => {
@@ -25,27 +36,29 @@ export const ProductMgmt = () => {
     }, [])
 
     return (
-        <div className="mgmtPage">
-            <button onClick={goToAddForm} id="addProdBtn">Add New Product</button>
-            <div className="sizingBox">
-                <table className="productList">
-                    <thead>
-                        <tr className="tableSubLabel">
-                            <th>Name</th>
-                            <th>Quantity</th>
-                            <th>Cost</th>
-                            <th>Sell Price</th>
-                            <th>Profit</th>
-                            <th>Controls</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {products.map(product => {
-                            return <ProductCard key={product.id} product={product} getProducts={getProducts} />
-                        })}
-                    </tbody>
-                </table>
+        <Draggable disabled={userIsOnMobile}>
+            <div className="mgmtPage">
+                <button onClick={goToAddForm} id="addProdBtn">Add New Product</button>
+                <div className="sizingBox">
+                    <table className="productList">
+                        <thead>
+                            <tr className="tableSubLabel">
+                                <th>Name</th>
+                                <th>Quantity</th>
+                                <th>Cost</th>
+                                <th>Sell Price</th>
+                                <th>Profit</th>
+                                <th>Controls</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {products.map(product => {
+                                return <ProductCard key={product.id} product={product} getProducts={getProducts} />
+                            })}
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+        </Draggable>
     )
 }
